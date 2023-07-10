c64a3f6b2913e4bfc8ae698c1817574264c72d72
ThaisTimoteo <130317756+ThaisTimoteo@users.noreply.github.com>
2023-07-10 00:43:49 +0000

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import yfinance as yf
+import plotly.graph_objects as go
+# Titulo do app
+st.title(AppDeAções')
+# Barra lateral
+st.sidebar.title('Selecione o stock')
+ticker_symbol = st.sidebar.text_input('stock', 'AAPL', max_chars=10)
+# Baixar dados
+data = yf.download(ticker_symbol, start='2020-01-01', end =
+'2023-06-26')
+# Exibir os dados
+st.subheader('Histórico')
+st.dataframe(data)
+# Exibir Gráfico
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=data.index, y = data['Close'], name =
+'Fechamento'))
+fig.update_layout(title = f"{ticker_symbol}", xaxis_title = "Data",
+yaxis_title = "Preço")
+st.plotly_chart(fig)
