e9eb7209109721af413fe63273b1f96822e471c6
ThaisTimoteo <130317756+ThaisTimoteo@users.noreply.github.com>
2023-07-16 16:10:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import streamlit as st
 import yfinance as yf
 import plotly.graph_objects as go
-# Titulo do app
+import talib
+
+# Título do app
 st.title('AppDeAções')
 # Barra lateral
 st.sidebar.title('Selecione o stock')
 ticker_symbol = st.sidebar.text_input('stock', 'AAPL', max_chars=10)
 # Baixar dados
-data = yf.download(ticker_symbol, start='2020-01-01', end =
-'2023-06-26')
+data = yf.download(ticker_symbol, start='2020-01-01', end='2023-06-26')
+# Cálculo do indicador MACD
+macd, signal, hist = talib.MACD(data['Close'])
 # Exibir os dados
 st.subheader('Histórico')
 st.dataframe(data)
-# Exibir Gráfico
+# Exibir Gráfico com indicador MACD
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=data.index, y = data['Close'], name =
-'Fechamento'))
-fig.update_layout(title = f"{ticker_symbol}", xaxis_title = "Data",
-yaxis_title = "Preço")
+fig.add_trace(go.Scatter(x=data.index, y=data['Close'], name='Fechamento'))
+
+fig.add_trace(go.Scatter(x=data.index, y=macd, name='MACD'))
+fig.add_trace(go.Scatter(x=data.index, y=signal, name='Sinal'))
+fig.add_trace(go.Scatter(x=data.index, y=hist, name='Histograma'))
+
+fig.update_layout(title=f"{ticker_symbol}", xaxis_title="Data", yaxis_title="Preço")
 st.plotly_chart(fig)
